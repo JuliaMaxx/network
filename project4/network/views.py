be88b14d63d2b66f98f06118823602296f625eb7
Julia Maxy <jjuliamaxxx@gmail.com>
2023-07-03 13:46:54 +0300

    pagination v3

--- project4/network/views.py
+++ project4/network/views.py
@@ -15,7 +15,7 @@ def index(request):
     if request.method == "GET":
         posts = Post.objects.all()
         posts = posts.order_by('-time')
-        paginator = Paginator(posts, 5)
+        paginator = Paginator(posts, 10)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, "network/index.html", {
